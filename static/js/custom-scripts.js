5c57aba84f7faae614309ebaaa7991db4d050f66
agatamalucha <agata.malucha@gmail.com>
2019-05-25 16:23:07 +0100

    cards

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -1,7 +1,29 @@
 
-  $(document).ready(function(){
+$('.dropdown-trigger').dropdown();
+
+
+$(".alert").delay(3000).fadeOut(200, function() {
+    $(this).alert('close');
+});
+
+
+
+$(document).ready(function() {
     $('.sidenav').sidenav();
-  });
+
+    $('.deleteButton').on('click', function(){
+
+         var algorithm_id = $(this).attr('algorithm_id');
+
+         req = $.ajax({
+            url : '/delete',
+            type : 'DELETE',
+            data : {id : algorithm_id }
+         });
+
+         $('#algorithmRow'+algorithm_id).remove();
 
 
-   $(".dropdown-trigger").dropdown();
+    });
+
+});
--- wines.py
+++ wines.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('example.html')
+	return render_template('index.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
